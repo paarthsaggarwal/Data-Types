d58652ee4e8b75667fecea0761c2b2b526f2be79
paarthsaggarwal <79054391+paarthsaggarwal@users.noreply.github.com>
2022-04-10 09:13:47 +0100

    Add files via upload

---
+++ data types in c++.cpp
@@ -0,0 +1,21 @@
+# include <iostream> 
+
+using namespace std;
+
+int main()
+
+{
+    string phrase = "Bob the Builder fights Peppa Pig";
+    char letter = 'p';  
+    bool isFemale = false;
+    double number = 1.787663;
+    int age = 100; 
+    bool isMale = true;
+    bool hasadog = false;
+    bool isarichperson = false;
+    bool isaround180cm = true;
+
+    cout << number << endl;
+   
+    return 0;
+}
